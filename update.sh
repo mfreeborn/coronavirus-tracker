712096b9858b88dcc5ec5629fd4510d9acf73efd
Michael Freeborn <michaelfreeborn1@gmail.com>
2020-04-06 19:47:11 +0100

    update.sh

--- update.sh
+++ update.sh
@@ -9,16 +9,32 @@ cd ./coronavirus-tracker
 
 # set up the virtual environment
 python3 -m venv .temp-covid-tracker
-source .temp-covid-tracker/bin/activate
+source ./.temp-covid-tracker/bin/activate
 
-pip install -qqq ipykernel
+pip install -q ipykernel
 ipython kernel install --user --name=.temp-covid-tracker
 
-pip install -qqq -r requirements.txt
+pip install -q -r requirements.txt
 
-jupyter nbconvert --to html --template basic --execute --TagRemovePreprocessor.remove_input_tags='{"hide_cell"}' --output-dir ./exported  --GatewayKernelManager.default_kernel_name=.temp-covid-tracker "Covid-19 Global Tracker.ipynb"
+# execute the notebook and convert it to html
+jupyter nbconvert --to html --template basic --execute --TagRemovePreprocessor.remove_input_tags='{"hide_cell"}' --output-dir ./exported  --ExecutePreprocessor.kernel_name=".temp-covid-tracker" "Covid-19 Global Tracker.ipynb"
 
-jupyter kernelspec remove .temp-covid-tracker
+# get rid of the now redundant kernel
+jupyter kernelspec remove .temp-covid-tracker -f
 
-# tidy up after outselves
-#rm -R ~/.temp
+# move back up and get ready to update the Github Pages blog
+cd ..
+git clone git@github.com:mfreeborn/mfreeborn.github.io.git
+cd ./mfreeborn.github.io
+
+# update the new blog post
+echo $'---\nlayout: notebook_post\n---\n<!-- markdownlint-disable MD041 -->\n{% raw %}\n' > ./_posts/2020-03-15-interactive-coronavirus-map-with-jupyter-notebook.md
+cat ../coronavirus-tracker/exported/"Covid-19 Global Tracker.html" >> ./_posts/2020-03-15-interactive-coronavirus-map-with-jupyter-notebook.md
+echo $'\n{% endraw %}' >> ./_posts/2020-03-15-interactive-coronavirus-map-with-jupyter-notebook.md
+
+# then push it up to Github
+git commit -a -m "auto update global covid tracker post"
+git push
+
+# tidy up after ourselves
+rm -rf ~/.temp
